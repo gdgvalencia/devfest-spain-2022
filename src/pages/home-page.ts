9f87b633962fb9e4024795e73efafba930fb61eb
Eduardo Sebastian <esebastian@altiora.es>
2022-10-16 21:30:32 +0200

    Update DevFest logo

--- src/pages/home-page.ts
+++ src/pages/home-page.ts
@@ -53,11 +53,11 @@ export class HomePage extends ReduxMixin(PolymerElement) {
 
         .hero-logo {
           --lazy-image-width: 100%;
-          --lazy-image-height: 76px;
+          --lazy-image-height: 116px;
           width: var(--lazy-image-width);
           height: var(--lazy-image-height);
-          max-width: 240px;
-          max-height: 76px;
+          max-width: 300px;
+          max-height: 116px;
         }
 
         .info-items {
