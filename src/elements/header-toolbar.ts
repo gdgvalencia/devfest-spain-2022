de9e8ad617afc5708b1e0eab439f288d07da77af
Eduardo Sebastian <esebastian@altiora.es>
2022-10-16 18:10:49 +0200

    Simplify header toolbar

--- src/elements/header-toolbar.ts
+++ src/elements/header-toolbar.ts
@@ -178,52 +178,10 @@ export class HeaderToolbar extends ReduxMixin(PolymerElement) {
             </paper-tab>
           </template>
 
-          <paper-tab class="signin-tab" on-click="signIn" link hidden$="[[signedIn]]">
-            [[signInText]]
-          </paper-tab>
-
           <a href$="[[ticketUrl]]" target="_blank" rel="noopener noreferrer">
             <paper-button class="buy-button" primary>[[buyTicket]]</paper-button>
           </a>
         </paper-tabs>
-
-        <notification-toggle></notification-toggle>
-
-        <paper-menu-button
-          class="auth-menu"
-          hidden$="[[!signedIn]]"
-          vertical-align="top"
-          horizontal-align="right"
-          no-animations
-          layout
-          horizontal
-          center-center
-        >
-          <div
-            class="profile-image"
-            slot="dropdown-trigger"
-            style$="background-image: url('[[user.data.photoURL]]')"
-          ></div>
-          <div class="dropdown-panel profile-details" slot="dropdown-content" layout horizontal>
-            <div
-              class="profile-image"
-              slot="dropdown-trigger"
-              self-center
-              style$="background-image: url('[[user.data.photoURL]]')"
-            ></div>
-            <div layout vertical center-justified>
-              <span class="profile-name">[[user.data.displayName]]</span>
-              <span class="profile-email">[[user.data.email]]</span>
-              <span class="profile-action" role="button" on-click="signOut">[[signOutText]]</span>
-            </div>
-          </div>
-        </paper-menu-button>
-
-        <paper-icon-button
-          icon="hoverboard:account"
-          on-click="signIn"
-          hidden$="[[isAccountIconHidden(signedIn, viewport.isLaptopPlus)]]"
-        ></paper-icon-button>
       </app-toolbar>
     `;
   }
